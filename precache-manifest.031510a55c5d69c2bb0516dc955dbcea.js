33d5d8fe3480df2c913176dd4945b4508e6a6549
Lars Karbo <mail@larskarbo.no>
2018-11-04 15:27:36 +0100

    Updates

---
+++ precache-manifest.031510a55c5d69c2bb0516dc955dbcea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "102ff033b736ffa45a11ee714e8d0cb9",
+    "url": "/static/media/ripple.102ff033.gif"
+  },
+  {
+    "revision": "c02a22e0acb08fac17145d2e49839d1e",
+    "url": "/static/media/hotel.c02a22e0.png"
+  },
+  {
+    "revision": "2c2e3c816b04900944b4d715b317ba07",
+    "url": "/static/media/3.2c2e3c81.png"
+  },
+  {
+    "revision": "0250b5740d73edc8bf20d5b2a5e19312",
+    "url": "/static/media/2.0250b574.png"
+  },
+  {
+    "revision": "b09e75e97048ba38d5a8ee29e9a0560f",
+    "url": "/static/media/1.b09e75e9.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f78083446370a323d74f",
+    "url": "/static/js/main.f7808344.chunk.js"
+  },
+  {
+    "revision": "5cb2bda23cbdef9ce78d",
+    "url": "/static/js/1.5cb2bda2.chunk.js"
+  },
+  {
+    "revision": "f78083446370a323d74f",
+    "url": "/static/css/main.c8fc8d13.chunk.css"
+  },
+  {
+    "revision": "6c7328b86f3c1603314a63cae4152bfe",
+    "url": "/index.html"
+  }
+];
