18b875f9a1f244078fa44171fd4402a2d449c228
Lars Karbo <mail@larskarbo.no>
2018-11-04 15:31:18 +0100

    Updates

---
+++ precache-manifest.796e90a309fdbdf6cb364675612e9702.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "102ff033b736ffa45a11ee714e8d0cb9",
+    "url": "/hackathon-trivastmago/static/media/ripple.102ff033.gif"
+  },
+  {
+    "revision": "c02a22e0acb08fac17145d2e49839d1e",
+    "url": "/hackathon-trivastmago/static/media/hotel.c02a22e0.png"
+  },
+  {
+    "revision": "2c2e3c816b04900944b4d715b317ba07",
+    "url": "/hackathon-trivastmago/static/media/3.2c2e3c81.png"
+  },
+  {
+    "revision": "0250b5740d73edc8bf20d5b2a5e19312",
+    "url": "/hackathon-trivastmago/static/media/2.0250b574.png"
+  },
+  {
+    "revision": "b09e75e97048ba38d5a8ee29e9a0560f",
+    "url": "/hackathon-trivastmago/static/media/1.b09e75e9.png"
+  },
+  {
+    "revision": "66046c7e6799159ed647",
+    "url": "/hackathon-trivastmago/static/js/runtime~main.66046c7e.js"
+  },
+  {
+    "revision": "9d656d146df5529be6f4",
+    "url": "/hackathon-trivastmago/static/js/main.9d656d14.chunk.js"
+  },
+  {
+    "revision": "9ec7750d36557eb853d7",
+    "url": "/hackathon-trivastmago/static/js/1.9ec7750d.chunk.js"
+  },
+  {
+    "revision": "9d656d146df5529be6f4",
+    "url": "/hackathon-trivastmago/static/css/main.c8fc8d13.chunk.css"
+  },
+  {
+    "revision": "bfb93e05b445f3875c7eb25450ffdaa8",
+    "url": "/hackathon-trivastmago/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.031510a55c5d69c2bb0516dc955dbcea.js"
+  "/hackathon-trivastmago/precache-manifest.796e90a309fdbdf6cb364675612e9702.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/hackathon-trivastmago/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
